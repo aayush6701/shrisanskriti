532bc105c5015a20432d54265b36302ccc5b9955
Aayush <aayuch.faltu@gmail.com>
2025-09-17 00:32:43 +0530

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -10,7 +10,7 @@ import { X, ArrowLeft, ArrowRight, Download, Share2, Printer } from "lucide-reac
 import Loader from "../components/Loader";
 
 const defaultImages = [
-  "/gp.jpg",  "/i1.jpg", "/i3.jpg", "/i4.jpg", "/i5.jpg", "/i6.jpg",
+  "/gp.jpg",  "/i1.JPG", "/i3.JPG", "/i4.JPG", "/i5.JPG", "/i6.JPG",
 ];
 
 const aiImages = [
